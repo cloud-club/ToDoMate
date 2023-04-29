1a17c66647751d220a8088798016ef21c40983a5
minyou2675 <kymyy2675@gmail.com>
2023-04-29 16:07:07 +0900

    url 경로 추가

--- ToDoMate/mainApp/urls.py
+++ ToDoMate/mainApp/urls.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from .views import main
 urlpatterns = [
+    path(''),
     path('test',main),
+    path('month/<int:month>/'),
+    path('day/'),
+    path('<int:pk>/'),
 ]
