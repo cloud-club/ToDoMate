1075d7706c8488568846ef3bb3f9b40cb67a3fee
minyou2675 <kymyy2675@gmail.com>
2023-04-29 16:47:48 +0900

    view queryset 추가

--- ToDoMate/mainApp/views.py
+++ ToDoMate/mainApp/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.response import Response
+from .models import Schedule
+from .serializers import ScheduleSerializer
 # Create your views here.
 
 def main(request):
     return HttpResponse("안녕")
+
+
+
+def findMonth(month):
+    queryset = Schedule.objects.filter(month=month, is_active=True)
+    lst = ScheduleSerializer(queryset)
+        
+        
+def findByDay(month,day):
+    queryset = Schedule.objects.filter(month=month,day=day,is_active=True)
